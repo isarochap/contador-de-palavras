bbd8bec20426e1bbf62937d54b4514afed573096
Isabella Rocha <isa.rochap@gmail.com>
2017-11-29 20:42:23 -0200

    upload

--- src/main.c
+++ src/main.c
@@ -1,22 +1,54 @@
-/* Contador de palavras
- *
- * Este programa recebera uma serie de caracteres representando palavras em sua
- * entrada. Ao receber um caractere fim de linha ('\n'), deve imprimir na tela o
- * numero de palavras separadas que recebeu e, apos, encerrar.
- */
-
 #include <stdio.h>
 
+#define MAX 500
+
+int ehNumero(char c) {
+	return ((int)c >= 48 && (int)c <= 57);
+}
+
+int ehLetra(char c) {
+	return (((int)c >= 65 && (int)c <= 90) || ((int)c >= 97 && (int)c <= 122));
+}
+
+int ehSeparador(char c) {
+	return ((int)c == '.' || (int)c == ',');
+}
+
+
 int main() {
+	char frase[MAX];
 
-  char c;
+	gets(frase);
 
-  c = 1;
+	int palavras = 0;
 
-  while (c != '\n') {
-    scanf("%c", &c);
-  }
+	int ultimoFoiLetra = 0;
+	int ultimoFoiNumero = 0;
+	int ultimoFoiSeparador = 0;
 
-  printf("1\n");
-  return 0;
+	for (int i=1; frase[i] != '\0' && i<MAX; i++) {
+
+		if (ehLetra(frase[i]) && !ultimoFoiLetra) {
+			ultimoFoiLetra = 1;
+			palavras++;
+		}
+
+		if (!ehLetra(frase[i])) ultimoFoiLetra = 0;
+		else ultimoFoiLetra = 1;
+
+		if (ehNumero(frase[i]) && !ultimoFoiNumero && !ultimoFoiSeparador) {
+			ultimoFoiNumero = 1;
+			palavras++;
+		}
+
+		if (!ehNumero(frase[i])) ultimoFoiNumero = 0;
+		else ultimoFoiNumero = 1;
+
+		if (ehSeparador(frase[i])) ultimoFoiSeparador = 1;
+		else ultimoFoiSeparador = 0;
+
+	}
+
+	printf("%d\n", palavras);
+	return 0;
 }
